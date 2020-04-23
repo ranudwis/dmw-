2db56cb7b4fca521853f72c05aaad25163488e99
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-04-23 10:52:26 +0700

    Compress cover image and add another compressed cover image for default in webtutor

--- frontend/src/templates/webtutor/WebtutorList.vue
+++ frontend/src/templates/webtutor/WebtutorList.vue
@@ -2,7 +2,7 @@
     <v-row>
         <v-col v-for="webtutor in webtutors" :key="webtutor.id" cols="3">
             <v-card :to="{ name: 'webtutor.show', params: { slug: webtutor.slug } }">
-                <v-img src="@/assets/images/cover.jpg" max-height="200px" position="top"></v-img>
+                <v-img src="@/assets/images/webtutorDefaultCover.jpg" max-height="200px" position="top"></v-img>
                 <v-card-title>{{ webtutor.title }}</v-card-title>
                 <v-card-subtitle>{{ webtutor.posted_at }}</v-card-subtitle>
                 <v-card-text>
