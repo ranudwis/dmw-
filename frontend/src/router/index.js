f516243aa140c3fdd3efe46850ae0efd7122d6e7
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-04-28 08:30:47 +0700

    Change scroll behavior when changing route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -24,7 +24,10 @@ const routes = [
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        return savedPosition ? savedPosition : { x: 0, y: 0 }
+    }
 })
 
 export default router
