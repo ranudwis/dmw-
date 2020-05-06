0c61cd29e11dd9ac50219cd0276ba11340ecbf51
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-05-07 05:41:50 +0700

    Update dashboard navigation drawer links

--- frontend/src/templates/dashboard/NavigationDrawer.vue
+++ frontend/src/templates/dashboard/NavigationDrawer.vue
@@ -1,21 +1,29 @@
 <template>
-    <v-navigation-drawer app clipped>
+    <v-navigation-drawer app width="280" clipped>
         <v-list nav>
-            <v-list-item
+            <template
                 v-for="(navigation, i) in navigations"
-                :key="i"
-                :to="navigation.to"
-                :exact="navigation.exact"
-                link
             >
-                <v-list-item-icon>
-                    <v-icon>{{ navigation.icon }}</v-icon>
-                </v-list-item-icon>
+                <v-subheader v-if="typeof navigation === 'string'" :key="i">
+                    {{ navigation }}
+                </v-subheader>
 
-                <v-list-item-content>
-                    <v-list-item-title>{{ navigation.text }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
+                <v-list-item
+                    v-else
+                    :key="i"
+                    :to="navigation.to"
+                    :exact="navigation.exact"
+                    link
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ navigation.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ navigation.text }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </template>
         </v-list>
     </v-navigation-drawer>
 </template>
@@ -31,13 +39,42 @@ export default {
                     icon: 'mdi-view-dashboard',
                     text: 'Dashboard'
                 }, {
-                    to: {},
+                    to: { },
                     icon: 'mdi-head-question',
                     text: 'Ujian'
                 }, {
-                    to: {},
+                    to: { },
+                    icon: 'mdi-cog',
+                    text: 'Pengaturan DMW++'
+                }, {
+                    to: { },
+                    icon: 'mdi-clock',
+                    text: 'Antrian'
+                }, "Webtutor", {
+                    to: { name: 'home' },
+                    exact: true,
+                    icon: 'mdi-tag',
+                    text: 'Label'
+                }, {
+                    to: { },
+                    icon: 'mdi-web-clock',
+                    text: 'Menunggu Persetujuan'
+                }, {
+                    to: { },
                     icon: 'mdi-web',
-                    text: 'Webtutor',
+                    text: 'Artikel'
+                }, "Akademis", {
+                    to: { },
+                    icon: 'mdi-view-agenda',
+                    text: 'Semester'
+                }, {
+                    to: { },
+                    icon: 'mdi-file-clock',
+                    text: 'Menunggu persetujuan',
+                }, {
+                    to: { },
+                    icon: 'mdi-view-list',
+                    text: 'Mata kuliah'
                 }
             ]
         }
