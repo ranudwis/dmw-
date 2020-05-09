ed0460c57712c2c518bc9f8ccbdf2f1df1534977
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-05-09 12:09:29 +0700

    Create label test for index and existance check

--- tests/LabelTest.php
+++ tests/LabelTest.php
@@ -14,17 +14,75 @@ class LabelTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
+
         $this->labelName = $this->faker->word;
     }
 
-    public function testCanCreateLabel()
+    private function postLabel()
     {
         $this->json('POST', 'label', [
             'name' => $this->labelName
         ]);
+    }
+
+    public function testCanCreateLabel()
+    {
+        $this->postLabel();
 
         $this->seeJsonEquals([
             'created' => true
+        ]);
+        $this->seeInDatabase('labels', [
+            'name' => $this->labelName
+        ]);
+
+        $this->json('GET', 'label');
+    }
+
+    /**
+     * @depends testCanCreateLabel
+     */
+    public function testCanIndexLabel()
+    {
+        $this->postLabel();
+
+        $this->json('GET', 'label');
+
+        $this->seeJsonEquals([
+            'labels' => [
+                [
+                    'id' => '1',
+                    'name' => $this->labelName
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @depends testCanCreateLabel
+     */
+    public function testCanCheckExistance()
+    {
+        $this->postLabel();
+
+        $this->json('GET', 'label/' . $this->labelName . '/isExists');
+
+        $this->seeJsonEquals([
+            'exists' => true
+        ]);
+    }
+
+    /**
+     * @depends testCanCreateLabel
+     */
+    public function testCanCheckNotExistance()
+    {
+        $this->postLabel();
+
+        $this->json('GET', 'label/someRandomLabel/isExists');
+
+        $this->seeJsonEquals([
+            'exists' => false
         ]);
     }
 }
