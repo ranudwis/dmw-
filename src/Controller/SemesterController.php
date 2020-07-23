c48219caf48fe69d5e4cbd8b112651ee2fcd487b
Ranu Dwi Setiadi <ranudwis@gmail.com>
2020-07-23 19:05:36 +0700

    Create endpoint to show semester by slug

--- src/Controller/SemesterController.php
+++ src/Controller/SemesterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Semester;
 use App\Repository\SemesterRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -37,6 +39,17 @@ class SemesterController extends Controller
         $semesters = $this->repository->getAllWithCourses();
 
         return $this->jsonResponse(compact('semesters'), [
+            'ignores' => ['semester']
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}")
+     * @ParamConverter("semester", options={"mapping": {"slug": "slug"}})
+     */
+    public function show(Semester $semester)
+    {
+        return $this->jsonResponse($semester, [
             'ignores' => ['semester']
         ]);
     }
--- tests/Controller/SemesterControllerTest.php
+++ tests/Controller/SemesterControllerTest.php
@@ -3,9 +3,19 @@
 namespace App\Tests\Controller;
 
 use App\Tests\TestCase;
+use App\Repository\SemesterRepository;
 
 class SemesterControllerTest extends TestCase
 {
+    private $repository;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->repository = static::$container->get(SemesterRepository::class);
+    }
+
     public function testCanIndexSemester()
     {
         $this->client->xmlHttpRequest('GET', 'semester');
@@ -20,5 +30,24 @@ class SemesterControllerTest extends TestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertJsonStructure([ 'semesters' ]);
+    }
+
+    public function testCanShowSemesterBySlug()
+    {
+        $semester = $this->repository->createQueryBuilder('s')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getSingleResult();
+
+        $this->client->xmlHttpRequest('GET', 'semester/' . $semester->getSlug());
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonStructure([
+            'id',
+            'type',
+            'slug',
+            'title',
+            'courses'
+        ]);
     }
 }
